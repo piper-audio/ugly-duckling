7e242860ea195c5cbc0084f387dd2f261b1e1979
Lucas Thompson <dev@lucas.im>
2016-10-27 08:20:49 +0100

    Get some wave-ui layers rendering inside the waveform component, a real shame about the global scope of the waves-ui library currently.

--- src/app/waveform/waveform.component.ts
+++ src/app/waveform/waveform.component.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import {
+  Component, OnInit, ViewChild, ElementRef,
+  AfterViewInit
+} from '@angular/core';
 
 declare var wavesUI: any;
 
@@ -7,14 +10,30 @@ declare var wavesUI: any;
   templateUrl: './waveform.component.html',
   styleUrls: ['./waveform.component.css']
 })
-export class WaveformComponent implements OnInit {
+export class WaveformComponent implements OnInit, AfterViewInit {
   @ViewChild('track') trackDiv: ElementRef;
 
   constructor() {
     console.log(wavesUI.core);
   }
 
-  ngOnInit() {
-    console.log(this.trackDiv.nativeElement.getBoundingClientRect().width);
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    const track: HTMLElement = this.trackDiv.nativeElement;
+    const duration: number = 1.0;
+    const height: number = track.getBoundingClientRect().height;
+    const width: number = track.getBoundingClientRect().width;
+    const pixelsPerSecond = width / duration;
+    const timeline: any = new wavesUI.core.Timeline(pixelsPerSecond, width);
+    timeline.createTrack(track, height, 'main');
+    // time axis
+    const timeAxis = new wavesUI.helpers.TimeAxisLayer({
+      height: height,
+      top: 10,
+      color: 'gray'
+    });
+    timeline.addLayer(timeAxis, 'main', 'default', true);
+    timeline.state = new wavesUI.states.CenteredZoomState(timeline);
   }
 }
