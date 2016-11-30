51ed66f96d7425220ece3999e270e679d86cfd9b
Lucas Thompson <dev@lucas.im>
2016-11-30 10:21:49 +0000

    wire up playback controls.

--- src/app/playback-control/playback-control.component.ts
+++ src/app/playback-control/playback-control.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AudioPlayerService} from "../services/audio-player.service";
 
 @Component({
   selector: 'app-playback-control',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaybackControlComponent implements OnInit {
 
-  constructor() { }
+  constructor(private audioService: AudioPlayerService) {
+  }
 
   ngOnInit() {
   }
 
+  emitPlayPause() {
+    this.audioService.togglePlaying();
+  }
+
+  emitFastForward() {
+    this.audioService.seekBy(5); // TODO this should probably be some dynamic amount based on the zoom level ala Sonic Visualiser
+  }
+
+  emitFastForwardEnd() {
+    this.audioService.seekToEnd();
+  }
+
+  emitFastRewind() {
+    this.audioService.seekBy(-5);
+  }
+
+  emitFastRewindStart() {
+    this.audioService.seekToStart();
+  }
+
+  emitVolumeChanged(value: number) {
+    this.audioService.setVolume(value);
+  }
+
+
+  // TODO seems wrong to be repeating myself
+  isPlaying(): boolean {
+    return this.audioService.isPlaying();
+  }
 }
