abbbe367011818578c8977392a708e8c82828ccc
Lucas Thompson <dev@lucas.im>
2016-12-02 18:56:13 +0000

    Load the generated scripts.js

--- bootstrap-feature-extraction-worker.js
+++ bootstrap-feature-extraction-worker.js
@@ -4,6 +4,6 @@
 let window = {};
 importScripts('inline.d41d8cd98f00b204e980.bundle.js'); // provides webpackJsonp
 const webpackJsonp = window['webpackJsonp'];
-importScripts('scripts.bundle.js'); // needs webpackJsonp, hence above - provides require
+importScripts('scripts.381a46ab36087561bf50.bundle.js'); // needs webpackJsonp, hence above - provides require
 
 new (require('feature-extraction-worker'))(self);
