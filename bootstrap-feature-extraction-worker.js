941c21db1c113340322c40567ca620290de358cf
Lucas Thompson <dev@lucas.im>
2016-12-02 18:54:50 +0000

    Load the generated inline.js

--- bootstrap-feature-extraction-worker.js
+++ bootstrap-feature-extraction-worker.js
@@ -2,7 +2,7 @@
  * Created by lucas on 01/12/2016.
  */
 let window = {};
-importScripts('inline.bundle.js'); // provides webpackJsonp
+importScripts('inline.d41d8cd98f00b204e980.bundle.js'); // provides webpackJsonp
 const webpackJsonp = window['webpackJsonp'];
 importScripts('scripts.bundle.js'); // needs webpackJsonp, hence above - provides require
 
