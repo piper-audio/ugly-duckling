89d84d5c528313d5c83833236f625676c12202b6
Lucas Thompson <dev@lucas.im>
2016-12-19 14:29:45 +0000

    Manually update scripts import inn worker. Need to sort out incorporating this into the build process.

--- bootstrap-feature-extraction-worker.js
+++ bootstrap-feature-extraction-worker.js
@@ -4,6 +4,6 @@
 let window = {};
 importScripts('inline.d41d8cd98f00b204e980.bundle.js'); // provides webpackJsonp
 const webpackJsonp = window['webpackJsonp'];
-importScripts('scripts.3c9bf1600b7e8510b6cf.bundle.js'); // needs webpackJsonp, hence above - provides require
+importScripts('scripts.1cd69ad45f0a25d2342e.bundle.js'); // needs webpackJsonp, hence above - provides require
 
 new (require('feature-extraction-worker'))(self);
