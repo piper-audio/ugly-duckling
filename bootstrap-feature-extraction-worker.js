4d3d1c77cf74e4833d8ee08f15f657dba97eb1d8
Lucas Thompson <dev@lucas.im>
2017-03-02 16:25:52 +0000

    Some UI tweaks and faster waveform

--- bootstrap-feature-extraction-worker.js
+++ bootstrap-feature-extraction-worker.js
@@ -2,9 +2,9 @@
  * Created by lucas on 01/12/2016.
  */
 let window = {};
-importScripts('inline.928e2270e066a21e5ace.bundle.js'); // provides webpackJsonp
+importScripts('inline.0c3c919f95c1030ed82f.bundle.js'); // provides webpackJsonp
 const webpackJsonp = window['webpackJsonp'];
-importScripts('scripts.8848ad4ed57b82b36e10.bundle.js'); // needs webpackJsonp, hence above - provides RequireJS
-importScripts('polyfills.af8c36cc057c791425e1.bundle.js'); // provides the compiled FeatureExtractionWorker
+importScripts('scripts.3e994b74cf613c3b43f0.bundle.js'); // needs webpackJsonp, hence above - provides RequireJS
+importScripts('polyfills.9ec9c9a773e6a7381b94.bundle.js'); // provides the compiled FeatureExtractionWorker
 
 new (require('feature-extraction-worker'))(self, requirejs);
