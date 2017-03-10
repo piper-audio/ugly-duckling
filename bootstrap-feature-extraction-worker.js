47ad363acfa8ed1220c2b4409f3238aa99590b46
Lucas Thompson <dev@lucas.im>
2017-03-10 18:44:19 +0000

    Fix paths.

--- bootstrap-feature-extraction-worker.js
+++ bootstrap-feature-extraction-worker.js
@@ -2,9 +2,9 @@
  * Created by lucas on 01/12/2016.
  */
 let window = {};
-importScripts('/inline.7b30793d52c3d3154ed4.bundle.js'); // provides webpackJsonp
+importScripts('inline.7b30793d52c3d3154ed4.bundle.js'); // provides webpackJsonp
 const webpackJsonp = window['webpackJsonp'];
-importScripts('/scripts.3e994b74cf613c3b43f0.bundle.js'); // needs webpackJsonp, hence above - provides RequireJS
-importScripts('/polyfills.ac1404ee29b9c8c3103f.bundle.js'); // provides the compiled FeatureExtractionWorker
+importScripts('scripts.3e994b74cf613c3b43f0.bundle.js'); // needs webpackJsonp, hence above - provides RequireJS
+importScripts('polyfills.ac1404ee29b9c8c3103f.bundle.js'); // provides the compiled FeatureExtractionWorker
 
 new (require('feature-extraction-worker'))(self, requirejs);
