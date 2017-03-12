6e7874f4cbb1b5e311e4aacada3ffa2b1d14e48f
Lucas Thompson <dev@lucas.im>
2017-03-12 23:53:10 +0000

    Latest waves-ui changes and keep play-head on top.

--- bootstrap-feature-extraction-worker.js
+++ bootstrap-feature-extraction-worker.js
@@ -2,7 +2,7 @@
  * Created by lucas on 01/12/2016.
  */
 let window = {};
-importScripts('inline.7b30793d52c3d3154ed4.bundle.js'); // provides webpackJsonp
+importScripts('inline.99598abf110e361fda56.bundle.js'); // provides webpackJsonp
 const webpackJsonp = window['webpackJsonp'];
 importScripts('scripts.3e994b74cf613c3b43f0.bundle.js'); // needs webpackJsonp, hence above - provides RequireJS
 importScripts('polyfills.ac1404ee29b9c8c3103f.bundle.js'); // provides the compiled FeatureExtractionWorker
