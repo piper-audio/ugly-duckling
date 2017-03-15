e2aed65af9d68cf44fc01f8435ddd2824027b076
Lucas Thompson <dev@lucas.im>
2017-03-15 12:09:50 +0000

    Start writing type definitions for waves-ui - this may eventually belong in waves-ui itself.

--- src/app/waveform/waves-ui.d.ts
+++ src/app/waveform/waves-ui.d.ts
@@ -1,4 +1,39 @@
 /**
  * Created by lucas on 01/12/2016.
  */
-declare module 'waves-ui';
+declare const AmbientInstance: WavesUserInterface;
+
+declare module 'waves-ui' {
+  export default AmbientInstance;
+}
+
+interface WavesUserInterface {
+  core: any;
+  helpers: any;
+  states: any;
+  utils: Utilities;
+}
+
+interface MatrixEntity {
+  getColumnCount: () => number;
+  getColumnHeight: () => number;
+  getColumn: (n: number) => number[];
+  getStepDuration: () => number;
+  getStartTime: () => number;
+}
+
+interface MatrixEntityConstructor {
+  new(): MatrixEntity;
+}
+
+interface PrefilledMatrixEntityConstructor {
+  new(data: Float32Array[] | number[][],
+      startTime: number,
+      stepDuration: number): MatrixEntity;
+}
+
+interface Utilities {
+  MatrixEntity: MatrixEntityConstructor;
+  PrefilledMatrixEntity: PrefilledMatrixEntityConstructor;
+  scales: any;
+}
