4102489c64bcea4bcdb0b94a815e48f3c0613728
Lucas Thompson <dev@lucas.im>
2017-03-15 15:21:00 +0000

    First pass at Layer type definition.

--- src/app/waveform/waves-ui.d.ts
+++ src/app/waveform/waves-ui.d.ts
@@ -1,3 +1,4 @@
+import EventEmitter = NodeJS.EventEmitter;
 /**
  * Created by lucas on 01/12/2016.
  */
@@ -8,7 +9,7 @@ declare module 'waves-ui' {
 }
 
 interface WavesUserInterface {
-  core: any;
+  core: Core;
   helpers: any;
   states: any;
   utils: Utilities;
@@ -20,6 +21,62 @@ interface MatrixEntity {
   getColumn(n: number): Float32Array | number[];
   getStepDuration(): number;
   getStartTime(): number;
+}
+
+type TimeContext = any; // TODO
+
+interface Area {
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+}
+
+interface Layer extends EventEmitter {
+  destroy(): void;
+  configureTimeContextBehaviour(ctor: ObjectConstructor): void;
+  start: number;
+  offset: number;
+  duration: number;
+  stretchRatio: number;
+  yDomain: number[];
+  opacity: number;
+  readonly timeToPixel: () => (time: number) => number;
+  readonly valueToPixel: () => (value: number) => number;
+  readonly items: Element[];
+  data: ArrayLike<any> | Object;
+  setTimeContext(context: TimeContext): void;
+  configureShape(ctor: ObjectConstructor /* TODO BaseShape*/,
+                 accessors: Object,
+                 options: Object): void;
+  configureCommonShape(ctor: ObjectConstructor /* TODO BaseShape*/,
+                       accessors: Object,
+                       options: Object): void;
+  setBehaviour(behaviour: Object /* TODO BaseBehavior */): void;
+  readonly selectedItems: Element[];
+  select(...$items: Element[]);
+  unselect(...$items: Element[]);
+  toggleSelection(...$items: Element[]);
+  edit($items: Element[], dx: number, dy: number, $target: Element): void;
+  setContextEditable(bool: boolean): void;
+  editContext(dx: number, dy: number, $target: Element): void;
+  stretchContext(dx: number, dy: number, $target: Element): void;
+  getItemFromDOMElement($el: Element): Element | null;
+  getDatumFromItem($item: Element): Object | any[] | null;
+  getDatumFromDOMElement($item: Element): Object | any[] | null;
+  hasItem($item: Element): boolean;
+  hasElement($el: Element): boolean;
+  getItemsInArea(area: Area): Element[];
+  render(): void;
+  update(): void;
+  updateContainer(): void;
+  updateShapes(): void;
+}
+
+interface LayerConstructor {
+  new(dataType: "entity" | "collection",
+      data: ArrayLike<any> | Object,
+      options: Object): Layer;
 }
 
 interface MatrixEntityConstructor {
@@ -36,4 +93,10 @@ interface Utilities {
   MatrixEntity: MatrixEntityConstructor;
   PrefilledMatrixEntity: PrefilledMatrixEntityConstructor;
   scales: any;
+}
+
+interface Core {
+  Layer: LayerConstructor;
+  LayerTimeContext: any; // TODO
+  Timeline: any; // TODO
 }
