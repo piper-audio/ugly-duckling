87ad95c21b0ef6559f324c11b32d34f18210fa1c
Lucas Thompson <dev@lucas.im>
2017-03-21 14:01:28 +0000

    Seeking with click on desktop. Remove old tap gesture code for mobile, as this should provide the same functionality.

--- src/app/waveform/waveform.component.ts
+++ src/app/waveform/waveform.component.ts
@@ -344,11 +344,6 @@ export class WaveformComponent implements OnInit, AfterViewInit, OnDestroy {
         this.timeline.timeContext.offset += newCenterTime - lastCenterTime;
         this.timeline.tracks.update();
       };
-      const seek = (ev) => {
-        this.audioService.seekTo(
-          this.timeline.timeContext.timeToPixel.invert(ev.center.x) - this.timeline.timeContext.offset
-        );
-      };
       hammertime.get('pinch').set({ enable: true });
       hammertime.on('panstart', () => {
         this.offsetAtPanStart = this.timeline.timeContext.offset;
@@ -370,7 +365,6 @@ export class WaveformComponent implements OnInit, AfterViewInit, OnDestroy {
       hammertime.on('pinchend', () => {
         zoomGestureJustEnded = true;
       });
-      hammertime.on('tap', seek);
     }
 
     this.animate();
@@ -640,5 +634,14 @@ export class WaveformComponent implements OnInit, AfterViewInit, OnDestroy {
     this.featureExtractionSubscription.unsubscribe();
     this.playingStateSubscription.unsubscribe();
     this.seekedSubscription.unsubscribe();
+  }
+
+  seek(x: number): void {
+    if (this.timeline) {
+      const timeContext: any = this.timeline.timeContext;
+      this.audioService.seekTo(
+        timeContext.timeToPixel.invert(x)- timeContext.offset
+      );
+    }
   }
 }
