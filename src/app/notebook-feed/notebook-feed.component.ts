22b38cafd4995abac38b07b869b26af1aa581995
Lucas Thompson <dev@lucas.im>
2017-03-22 11:14:40 +0000

    Extend TimelineTimeContext so emit events when changing offset and zoom

--- src/app/notebook-feed/notebook-feed.component.ts
+++ src/app/notebook-feed/notebook-feed.component.ts
@@ -2,6 +2,49 @@
  * Created by lucast on 21/03/2017.
  */
 import {Component, Input} from "@angular/core";
+import Waves from 'waves-ui';
+import {EventEmitter} from 'events';
+
+export interface PartialEventEmitter {
+  on(event: string|symbol, listener: Function): void;
+}
+
+class NotifyingTimeContext extends Waves.core.TimelineTimeContext
+  implements PartialEventEmitter {
+
+  private eventEmitter: EventEmitter;
+
+  constructor(pixelsPerSecond: number, visibleWidth: number) {
+    super(pixelsPerSecond, visibleWidth);
+    this.eventEmitter = new EventEmitter();
+  }
+
+  get offset(){
+    return super.offset;
+  }
+
+  set offset(value: number) {
+    if (value !== this.offset) {
+      this.eventEmitter.emit('offset');
+    }
+    super.offset = value;
+  }
+
+  get zoom(){
+    return super.zoom;
+  }
+
+  set zoom(value: number) {
+    if (value !== this.zoom) {
+      this.eventEmitter.emit('zoom');
+    }
+    super.zoom = value;
+  }
+
+  on(event: string|symbol, listener: Function): void {
+    this.eventEmitter.on(event, listener);
+  }
+}
 
 @Component({
   selector: 'ugly-notebook-feed',
