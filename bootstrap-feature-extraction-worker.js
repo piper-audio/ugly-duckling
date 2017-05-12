aeff4a9ec41519f7d105ec59216acf832e117651
Lucas Thompson <dev@lucas.im>
2017-05-12 17:40:35 +0100

    Try again

--- bootstrap-feature-extraction-worker.js
+++ bootstrap-feature-extraction-worker.js
@@ -5,7 +5,7 @@ let window = {};
 importScripts('inline.bundle.js'); // provides webpackJsonp
 const webpackJsonp = window['webpackJsonp'];
 importScripts('scripts.bundle.js'); // needs webpackJsonp, hence above - provides RequireJS
-window = null; // now we have a reference to webpackJsonp, get rid of window
+window = null;  // now we have a reference to webpackJsonp, get rid of window
 importScripts('polyfills.bundle.js'); // provides the compiled FeatureExtractionWorker
 
 new (require('feature-extraction-worker'))(self, requirejs);
